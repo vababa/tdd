c8bd51a2b29e71d275712e6e56093b2dc696b5bf
Vladimir Babaev <babay_ka@mail.ru>
2018-04-18 17:15:22 +0300

    Second commit

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,22 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox(executable_path=r'C:\Users\Svetlana\PycharmProjects\superlists\geckodriver.exe')
-browser.get('http://localhost:8000')
+class NewVisitorTest(unittest.TestCase):
 
-assert 'Django' in browser.title
+    def setUp(self):
+        self.browser = webdriver.Firefox(executable_path=r'C:\Users\Svetlana\PycharmProjects\superlists\geckodriver.exe')
+        self.browser.implicitly_wait(3)
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        self.browser.get('http://localhost:8000')
+
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('Finish the test!')
+
+
+if __name__ == "__main__":
+    unittest.main(warnings='ignore')
+
+
